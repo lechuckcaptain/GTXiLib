ce64f13a8e001fa6728a01ad4df3b1723a202a26
Tomas Camin <tomas.camin@adevinta.com>
2023-01-12 09:05:01 +0100

    Add Sample project

---
+++ Sample/GTXSample/SomeClass.h
@@ -0,0 +1,16 @@
+//
+//  SomeClass.h
+//  GTXSample
+//
+//  Created by tomas on 11/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SomeClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sample/GTXSample/SomeClass.m
@@ -0,0 +1,12 @@
+//
+//  SomeClass.m
+//  GTXSample
+//
+//  Created by tomas on 11/01/23.
+//
+
+#import "SomeClass.h"
+
+@implementation SomeClass
+
+@end
